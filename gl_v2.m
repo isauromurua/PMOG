ea83da576455ba70a89119178abd466df711b29f
isauromurua <macbookpro@Javiers-MacBook-Pro.local>
2019-02-13 12:28:24 -0600

    e

--- gl_v2.m
+++ gl_v2.m
@@ -1,8 +1,41 @@
+function gl_v2(x,y,z,lambda,phi0,m,n,conj)
+% 
+% 
 % 
 
-function =
+% =================  Gaussian profile   =====================
 
-curve = 
+
+
+% =================  Guoy's phase   =====================
+
+
+
+% =================  Curved wavefront   =====================
+
+
+function w = waist(z)
+% Returns the waist of the beam at distance z of propagation.
+w = (1/pi)*sqrt(lambda.^2*z.^2+pi^2w0^2);
+
+% =================  Laguerre   =====================
+
+
+
+function y = laguerg(a,n,x)
+% 
+% 
+% 
 
 
 % 
+if mod(n,2) == 0
+    impares = repmat([1 -1],n/2,1);
+else
+    impares = repmat([1,-1],2*(n-1)/2,1);
+end
+
+
+
+
+y = sum(suma);
