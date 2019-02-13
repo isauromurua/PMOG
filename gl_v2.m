29a53a1fb17796324d7fbc09901f30af61d75130
CR-Reyes <rcao717@gmail.com>
2019-02-13 13:23:21 -0600

    Agregamos estructura para programar las funciones

--- gl_v2.m
+++ gl_v2.m
@@ -1,41 +1,50 @@
-function gl_v2(x,y,z,lambda,phi0,m,n,conj)
-% 
-% 
-% 
+function gl_v2(x, y, z, lambda, w0, phi0, m, n, conj)
+    %
+    %
+    %
 
-% =================  Gaussian profile   =====================
-
-
-
-% =================  Guoy's phase   =====================
-
-
-
-% =================  Curved wavefront   =====================
-
-
-function w = waist(z)
-% Returns the waist of the beam at distance z of propagation.
-w = (1/pi)*sqrt(lambda.^2*z.^2+pi^2w0^2);
-
-% =================  Laguerre   =====================
-
-
-
-function y = laguerg(a,n,x)
-% 
-% 
-% 
-
-
-% 
-if mod(n,2) == 0
-    impares = repmat([1 -1],n/2,1);
-else
-    impares = repmat([1,-1],2*(n-1)/2,1);
 end
 
+% PARAXIAL WAVE FACTORS:
 
+% =================  1. Laguerre-Gauss modulator   =====================
 
+function y = laguerg(a, n, x)
+    %
+    %
+    %
+    if mod(n, 2) == 0
+        impares = repmat([1 - 1], n / 2, 1);
+    else
+        impares = repmat([1, -1], 2 * (n - 1) / 2, 1);
+    end
 
-y = sum(suma);
+    y = sum(suma);
+end
+
+% =================  2. Gaussian profile   =====================
+
+function w = waist(z)
+    % Returns the waist of the beam at distance z of propagation.
+    w = (1 / pi) * sqrt(lambda.^2 * z.^2 + pi^2 .* w0^2);
+end
+
+% =================  3. Curved wavefront   =====================
+
+function rayleigh_range = q0(lambda, w0)
+    %
+    rayleigh_range = pi * w0.^2 / lambda
+end
+    function curv_rad = R(z)
+        % returns the radius of curvature of the wavefront
+        curv_rad = z .* (1 + (q_0 ./ z).^2)
+    end
+
+    function cwf = curved_wavefront(z)
+        % Returns the curved wafvefront resulting from spherical
+        % wave distortion
+        Cur = k / (2 * R(z))
+        cwf = e.^(j .* term1 .* r_perp.^2)
+    end
+
+    % =================  4. Guoy's phase   =====================
